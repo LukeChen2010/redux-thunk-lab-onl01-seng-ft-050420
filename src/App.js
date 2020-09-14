7465af2f086c1e95416c7cfbbe35381eea8f405d
Luke Chen <luke.chen.2010@gmail.com>
2020-09-14 02:00:55 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 
-class App extends Component {   
+class App extends Component {  
+  
+  componentDidMount() {
+    this.props.fetchCats()
+  }
+  
+  handleLoading = () => {
+    console.log(this.props.loading)
+    if(this.props.loading) {
+      return <div>Loading...</div>
+    } else {
+      return <CatList catPics={this.props.catPics} />
+    }
+  }
   
   render() {
     return (
